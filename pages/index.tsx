d26d51042a5cf3dc18944ae3aed89b173b16a652
danielcsato <84443414+danielcsato@users.noreply.github.com>
2021-08-22 09:06:29 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Landing from "../components/landing/Landing";
-import styles from "../styles/Home.module.scss";
+import type { NextPage } from 'next';
+import Landing from '../components/landing/Landing';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
